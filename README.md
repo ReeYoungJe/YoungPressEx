647b8446e9bced931e46c34b7be49be6b21fcfe8
ReeYoungJe <zofhd2@naver.com>
2025-05-14 16:09:51 +0900

    확장자 html 도 ejs 쓰게 만들고 npm node 버전 올림

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ```
 익스프레스
-node v14.17.6
-npm v6.14.15
+node v22.15.0
+npm v10.9.2
+```
+
+
+```
+express API
+라우팅 https://expressjs.com/ko/guide/routing.html
 ```
--- server.js
+++ server.js
@@ -2,28 +2,44 @@ const express = require('express');
 const app = express();
 const port = 3002;
 const path = require('path');
-import ejs from "ejs";
 
-// 미들웨어 함수 - 디버깅 로그 추가
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');  // 기본 뷰 엔진을 html로 변경
+app.set('views', path.join(__dirname, 'src'));
+
 const filterByExtension = (allowedExtensions) => {
     return (req, res, next) => {
-        // 디버깅용 로그
         const ext = path.extname(req.path).toLowerCase();
-        // 주의: 허용된 확장자일 때 next()로 진행
         if (allowedExtensions.includes(ext)) {
             return next();
         }
-        // 허용되지 않은 확장자는 차단
         return res.status(403).send('원하는 확장자 아님');
     };
 };
 
-// CSS 폴더 설정
+// CSS와 JS 폴더 설정
 app.use('/css', filterByExtension(['.css']), express.static('src/css'));
-// JS 폴더 설정
 app.use('/js', filterByExtension(['.js']), express.static('src/js'));
-// Pages 폴더 설정
-app.use('/page', filterByExtension(['.html', '.ejs']), express.static('src/page'));
+app.use('/scss', filterByExtension(['.scss']), express.static('src/scss'))
+// HTML 파일 라우팅 처리
+app.get('/*.html', (req, res) => {
+    const fileName = path.basename(req.path, '.html');
+    res.render(fileName + '.html');
+});
+
+// 페이지 라우팅
+app.get('/page/*', (req, res) => {
+    const filePath = req.path.substring(1);
+    res.render(filePath);
+});
+
+// 루트 경로 처리
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+// 정적 파일 처리는 마지막에
+app.use(express.static('src'));
 
 app.listen(port, () => {
     console.log(`서버가 포트 ${port}에서 실행 중입니다`);
